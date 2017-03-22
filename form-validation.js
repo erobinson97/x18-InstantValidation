8072677fef2213f457376f37457d97862e977d7b
Ethan Robinson <erobinson11@mail.csuchico.edu>
2017-03-21 19:33:46 -0700

    finished

--- form-validation.js
+++ form-validation.js
@@ -1,29 +1,12 @@
-function validate() {
+function validatePassword() {
 
   //Grab the user's input and store in variables
-  var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
-  userEntered = userEntered.toLowerCase();
   passEntered = passEntered.toLowerCase();
 
-
-  if (checkSpaces(userEntered)) {
-      //Show message that there is an error with the username...
-      document.getElementById("usernameError").innerHTML = "There cannot be spaces in the username.";
-      document.getElementById("usernameError").classList.remove("hidden-message");
-      document.getElementById("usernameError").classList.add("shown-message");
-      //Turn the username items red
-      document.getElementById("usernameGroup").classList.add("has-error");
-  }
-  else if (userEntered.length < 6) {
-      //Show message that there is an error with the username...
-      document.getElementById("usernameError").innerHTML = "Username must have at least 6 characters.";
-      document.getElementById("usernameError").classList.remove("hidden-message");
-      document.getElementById("usernameError").classList.add("shown-message");
-      //Turn the username items red
-      document.getElementById("usernameGroup").classList.add("has-error");
-  }
-
+  var userEntered = document.getElementById("user").value;
+  userEntered = userEntered.toLowerCase();
+ 
   if (passEntered == userEntered) {
       //Show message that there is an error with the username...
       document.getElementById("usernameError").innerHTML = "Username and password cannot be the same.";
@@ -38,6 +21,7 @@ function validate() {
       document.getElementById("passwordError").classList.add("shown-message");
       //Turn the password items red
       document.getElementById("passwordGroup").classList.add("has-error");
+      document.getElementById("usernameGroup").classList.remove("has-success");
   }
   else if (passEntered == "password") {
 
@@ -47,6 +31,7 @@ function validate() {
       document.getElementById("passwordError").classList.add("shown-message");
       //Turn the password items red
       document.getElementById("passwordGroup").classList.add("has-error");
+      document.getElementById("usernameGroup").classList.remove("has-success");
   }
   else if (passEntered.length < 6) {
       //Show message that there is an error with the password...
@@ -55,6 +40,7 @@ function validate() {
       document.getElementById("passwordError").classList.add("shown-message");
       //Turn the password items red
       document.getElementById("passwordGroup").classList.add("has-error");
+      document.getElementById("usernameGroup").classList.remove("has-success");
   }
   else if (passEntered.length > 20) {
       //Show message that there is an error with the password...
@@ -63,6 +49,13 @@ function validate() {
       document.getElementById("passwordError").classList.add("shown-message");
       //Turn the password items red
       document.getElementById("passwordGroup").classList.add("has-error");
+      document.getElementById("usernameGroup").classList.remove("has-success");
+  }
+  else {
+      document.getElementById("passwordError").classList.remove("shown-message");
+      document.getElementById("passwordError").classList.add("hidden-message");
+      document.getElementById("passwordGroup").classList.add("has-success");
+      document.getElementById("passwordGroup").classList.remove("has-error");
   }
 }
 
@@ -72,4 +65,42 @@ function checkSpaces(str) {
             return true;
         }
     }
+}
+
+function validateUsername() {
+    var userEntered = document.getElementById("user").value;
+    userEntered = userEntered.toLowerCase();
+
+    if (checkSpaces(userEntered)) {
+        //Show message that there is an error with the username...
+        document.getElementById("usernameError").innerHTML = "There cannot be spaces in the username.";
+        document.getElementById("usernameError").classList.remove("hidden-message");
+        document.getElementById("usernameError").classList.add("shown-message");
+        //Turn the username items red
+        document.getElementById("usernameGroup").classList.add("has-error");
+        document.getElementById("usernameGroup").classList.remove("has-success");
+    }
+    else if (userEntered.length < 6) {
+        //Show message that there is an error with the username...
+        document.getElementById("usernameError").innerHTML = "Username must have at least 6 characters.";
+        document.getElementById("usernameError").classList.remove("hidden-message");
+        document.getElementById("usernameError").classList.add("shown-message");
+        //Turn the username items red
+        document.getElementById("usernameGroup").classList.add("has-error");
+        document.getElementById("usernameGroup").classList.remove("has-success");
+    }
+    else {
+        document.getElementById("usernameError").classList.remove("shown-message");
+        document.getElementById("usernameError").classList.add("hidden-message");
+        document.getElementById("usernameGroup").classList.add("has-success");
+        document.getElementById("usernameGroup").classList.remove("has-error");
+    }
+}
+
+function register() {
+    var passEntered = document.getElementById("pass").value;
+    var userEntered = document.getElementById("user").value;
+
+    console.log(userEntered);
+    console.log(passEntered);
 }
